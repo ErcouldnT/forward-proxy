2ce8998c1b733a7042f890c0a6252e1c6698b497
Ercode Techoglu <ercode@pm.me>
2024-01-15 22:46:24 +0300

    Forward proxy without any dependencies ✨

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,67 @@
-export const add = (a: number, b: number) => {
-  // Typescript will guess the function returns a number
-  return a + b;
-};
+import net from "net";
+export const server = net.createServer();
 
-const number1 = 5;
-const number2 = 10;
-const result = add(number1, number2); // Result will be a number
+server.on("connection", (clientToProxySocket) => {
+  console.log("Client connected to proxy");
+  clientToProxySocket.once("data", (data) => {
+    const isConnectionTLS = data.toString().indexOf("CONNECT") !== -1;
 
-console.log("The result is %d", result);
+    let serverPort = 80;
+    let serverAddress;
+
+    if (isConnectionTLS) {
+      serverPort = 443;
+
+      serverAddress = data.toString().split("CONNECT")[1].split(" ")[1].split(":")[0];
+    } else {
+      serverAddress = data.toString().split("Host: ")[1].split("\n")[0];
+    }
+
+    const proxyToServerSocket = net.createConnection(
+      {
+        host: serverAddress,
+        port: serverPort,
+      },
+      () => {
+        console.log("Proxy to server is setup");
+      },
+    );
+
+    if (isConnectionTLS) {
+      clientToProxySocket.write("HTTP/1.1 200 OK\r\n\n");
+    } else {
+      proxyToServerSocket.write(data);
+    }
+
+    clientToProxySocket.pipe(proxyToServerSocket);
+    proxyToServerSocket.pipe(clientToProxySocket);
+
+    proxyToServerSocket.on("error", (err) => {
+      console.log("Proxy to server error");
+      console.log(err);
+    });
+
+    clientToProxySocket.on("error", (err) => {
+      console.log("Client to proxy error" + err.message);
+    });
+  });
+});
+
+server.on("error", (err) => {
+  console.log("Internal server error");
+  console.log(err);
+});
+
+server.on("close", () => {
+  console.log("Client disconnected");
+});
+
+server.listen(
+  {
+    host: "0.0.0.0",
+    port: 8080,
+  },
+  () => {
+    console.log("Server listening on 0.0.0.0:8080");
+  },
+);
